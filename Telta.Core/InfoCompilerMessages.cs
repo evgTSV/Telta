a709777239e74aa21d79aa4e1dfa2884e7d731df
Evgeny608799 <freefiery@gmail.com>
2024-03-01 17:49:09 +0500

    Add Telta.Core and InfoCompilerMessages.cs

---
+++ Telta.Core/InfoCompilerMessages.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Telta.Core;
+
+public static class InfoCompilerMessages
+{
+    public static string CompilerBanner => $"Telta Compiler v{Assembly.GetExecutingAssembly().GetName().Version}";
+}
