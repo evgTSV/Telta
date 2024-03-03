213e75de22744cc7fca699d753a7a1f04406d84d
Evgeny608799 <freefiery@gmail.com>
2024-03-03 22:14:18 +0500

    Fixed warnings

--- Telta.Lexer/Position.fs
+++ Telta.Lexer/Position.fs
@@ -18,6 +18,8 @@ type public Position(line:int, column:int) =
     
     member this.CompareTo(other:Position) = (this :> IComparable<Position>).CompareTo(other)
     member this.CompareTo(other) = (this :> IComparable).CompareTo(other)
+    override this.Equals(other) = (this :> IComparable).CompareTo(other) = 0
+    override this.GetHashCode() = line ^^^ column
     
     static member op_Equality (pos1:Position, pos2:Position) = (pos1.CompareTo(pos2) = 0)
     static member op_Inequality (pos1:Position, pos2:Position) = (pos1.CompareTo(pos2) <> 0)
