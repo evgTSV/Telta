3157401c99aefd0e30e2593403bee429d1bfa1b5
Evgeny608799 <freefiery@gmail.com>
2024-03-13 00:24:59 +0500

    Add Ast project and declare some kinds of expressions

---
+++ Telta.Ast/Expressions.fs
@@ -0,0 +1,11 @@
+﻿namespace Telta.Ast
+
+open Telta.Lexer
+
+module Expressions =
+
+    type Expression =
+        | IdentifierExpression of identifier:string
+        | ParenExpression of contents:Token
+        | MemberCallExpression of ``member``:Expression * arguments:List<Expression>
+        | MemberAccessExpression of target:Expression * identifier:Expression
