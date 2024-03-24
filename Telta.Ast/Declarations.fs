4c1e2ed0d0ba2f54ccfae965b6e48d6c1c17fb72
Evgeny608799 <freefiery@gmail.com>
2024-03-24 23:54:37 +0500

    Update Declarations.fs

--- Telta.Ast/Declarations.fs
+++ Telta.Ast/Declarations.fs
@@ -1,6 +1,9 @@
 ﻿namespace Telta.Ast
 
+open Telta.Lexer
+
 module Declarations =
     
     type Declaration =
-        | MethodDeclaration
+        | ParameterDeclaration of identifier:Token * predefinedType:Token
+        | MethodDeclaration of identifier:Token * modifiers:List<Token> * parameters:List<Declaration>      
