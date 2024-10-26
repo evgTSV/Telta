33353dddd9f3a58760dd4946975e7105b7aa6e02
evgTSV <freefiery@gmail.com>
2024-10-26 20:48:13 +0500

    Fix tests 2

--- Telta.IntegrationTests/UnitTest1.cs
+++ Telta.IntegrationTests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace Telta.Compiler.Tests
+﻿namespace Telta.IntegrationTests;
+
+public class UnitTest1
 {
-    public class UnitTest1
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
-            Assert.True(true);
-        }
+
     }
 }
